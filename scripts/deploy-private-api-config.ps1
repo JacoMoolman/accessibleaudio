a5b731c995a1c8ebefe5b27cfbcc53b2dcb99375
Jaco Moolman <jacomoolman1890@gmail.com>
2026-07-17 18:42:46 +0200

    Fix FTPS certificate scope for private config

--- scripts/deploy-private-api-config.ps1
+++ scripts/deploy-private-api-config.ps1
@@ -12,7 +12,7 @@ foreach ($line in Get-Content -LiteralPath $EnvPath) {
   }
 }
 $hostValue = if ($values.FTP_IP) { $values.FTP_IP } else { $values.FTP_HOST }
-$allowedSuffix = if ($values.FTP_TLS_ALLOWED_CERT_SUFFIX) { $values.FTP_TLS_ALLOWED_CERT_SUFFIX.ToLowerInvariant() } else { ".hstgr.io" }
+$script:privateConfigTlsSuffix = if ($values.FTP_TLS_ALLOWED_CERT_SUFFIX) { $values.FTP_TLS_ALLOWED_CERT_SUFFIX.ToLowerInvariant() } else { ".hstgr.io" }
 $previousCertificateCallback = [System.Net.ServicePointManager]::ServerCertificateValidationCallback
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {
@@ -20,7 +20,7 @@ $previousCertificateCallback = [System.Net.ServicePointManager]::ServerCertifica
   if ($sslPolicyErrors -eq [System.Net.Security.SslPolicyErrors]::None) { return $true }
   if ($sslPolicyErrors -ne [System.Net.Security.SslPolicyErrors]::RemoteCertificateNameMismatch) { return $false }
   $dnsName = $certificate.GetNameInfo([System.Security.Cryptography.X509Certificates.X509NameType]::DnsName, $false)
-  return $dnsName.ToLowerInvariant().EndsWith($allowedSuffix)
+  return $dnsName.ToLowerInvariant().EndsWith($script:privateConfigTlsSuffix)
 }
 $request = [System.Net.FtpWebRequest]::Create("ftp://$hostValue/api/config.local.php")
 $request.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
